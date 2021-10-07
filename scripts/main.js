3fb9591bc0a71fc3935708fa4497763be2a375d3
Mlendea, Horațiu <Horatiu.Mlendea@ProtonMail.com>
2021-10-07 12:33:22 +0300

    Use a Google Chrome on Linux UA string

--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,7 @@ function createWindow () {
     }
   });
 
+  mainWindow.webContents.userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.63 Safari/537.36";
   mainWindow.loadURL('https://www.duolingo.com');
 }
 
