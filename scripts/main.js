f973a4ac03ef8e703933465e5ffb2a2507b92ec9
Horațiu Mlendea <io.github.hmlendea.cnzq9@SimpleLogin.fr>
2022-04-08 12:27:04 +0300

    Updated the user gent

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ function createWindow () {
     }
   });
 
-  mainWindow.webContents.userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36";
+  mainWindow.webContents.userAgent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36";
   mainWindow.loadURL('https://duolingo.com');
 }
 
